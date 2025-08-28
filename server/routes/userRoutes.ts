d2df5432bffbf80708a5baecdb6172839512f2de
sara <sury108683@gmail.com>
2025-08-28 10:39:52 +0300

    verifyEmail

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -2,6 +2,7 @@
  import  express from 'express';
 import { getUserProfileController ,handleUserUpsert,updateUserController} from '../controllers/userController';
 import { authenticate } from '../middleware/auth';
+import { verifyEmailMiddleware } from '../middleware/verifyEmail';
 
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post('/upsert', handleUserUpsert);
 router.patch('/updateMe/:uid', authenticate, updateUserController);
 router.get('/protected-route', authenticate, (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ message: 'Access granted', user: (req as any).user });
+});
+router.get("/protected", verifyEmailMiddleware, (req: Request, res: Response) => {
+  const user = (req as any).user;
+  res.json({ message: `Hello ${user.email}, you are verified!` });
 });
 export default router;
