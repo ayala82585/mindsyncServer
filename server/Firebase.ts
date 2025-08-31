a184d44b72ca348554afdbb7cb01204feeee058c
sara <sury108683@gmail.com>
2025-08-31 15:57:55 +0300

    mfa-totp

--- server/Firebase.ts
+++ server/Firebase.ts
@@ -4,24 +4,26 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 const serviceAccount = require('./mindsync-b978b-c1d1826e0375.json'); 
+// const svc = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!);
 
 // const serviceAccount = require('./mindsync-b978b-firebase-adminsdk-fbsvc-f0703ab54f.json');
 
 if(admin.apps.length === 0 ){
+    const path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+  if (!path) throw new Error('Missing GOOGLE_APPLICATION_CREDENTIALS path');
+  const svc = require(path);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(svc as admin.ServiceAccount),
 });}
 
 export async function verifyFirebaseToken(token: string): Promise<{ uid: string, email?: string, full_name?: string }> {
   try {
     const decoded = await admin.auth().verifyIdToken(token);
-    return {
-      uid: decoded.uid,
-      email: decoded.email,
-      full_name: decoded.full_name
-    };
+    return decoded;
   } catch (error) {
     throw new Error('Invalid Firebase token');
   }
 }
+export const verifyIdToken = (idToken: string) => admin.auth().verifyIdToken(idToken);
+
 export default admin;
